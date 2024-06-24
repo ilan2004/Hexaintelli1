72643864439937e584d94a0a36fc4b3720468c42
Ilan Usman <ilanusman03@gmail.com>
2024-06-25 03:55:07 +0530

    nav

--- src/Components/Navbar/Page.js
+++ src/Components/Navbar/Page.js
@@ -2,7 +2,6 @@
 
 import * as React from "react";
 import Link from "next/link";
-import Image from 'next/image';
 import { useState, useEffect } from 'react';
 import styles from './Navbar.module.css';
 
